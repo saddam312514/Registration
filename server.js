3f7872aef66c36fffea95797d53351c404afa5fa
saddam312514 <saddam312514@gmail.com>
2019-07-29 01:25:33 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express=require('express')
+
+var cors=require('cors')
+var bodyParser=require('body-parser')
+var mongoose=require('mongoose')
+var app=express()
+var port= process.env.PORT || 5000
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended:false
+    })
+)
+
+const mongoURI='mongodb://localhost:27017/login-Register'
+
+mongoose
+.connect(mongoURI,{useNewUrlParser:true})
+.then(()=>console.log('MongoDb Connected Successfully'))
+.catch(err=> console.log(err))
+
+
+var Users=require('./routes/Users')
+app.use('/users',Users)
+
+app.listen(port,()=>{
+    console.log('Server is running on port '+ port)
+})
+
+
